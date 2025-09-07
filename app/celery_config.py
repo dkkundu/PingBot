79a24c9166ce87e518ce0c71148bc8e8fda51db6
Dipto Kumar Kundu <dipto.kumar@sslwireless.com>
2025-09-07 15:04:11 +0600

    Update Celery config (ai:90)

--- app/celery_config.py
+++ app/celery_config.py
@@ -2,11 +2,28 @@ import os
 from celery import Celery
 
 # ---------------- INIT CELERY ----------------
-celery = Celery(
-    "notification_app",
-    broker=os.getenv("CELERY_BROKER_URL"),
-    backend=os.getenv("CELERY_BACKEND_URL")
-)
+
+using_redis_password = bool(int(os.getenv("USING_REDIS_PASSWORD")))
+
+if using_redis_password:
+    celery = Celery(
+        "notification_app",
+        broker=f"redis://:{os.getenv('REDIS_PASSWORD')}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}",
+        backend=f"redis://:{os.getenv('REDIS_PASSWORD')}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}"
+    )
+else:
+    celery = Celery(
+        "notification_app",
+        broker=f"redis://:{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}",
+        backend=f"redis://:{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/{os.getenv('REDIS_DB')}"
+    )
+
+
+# celery = Celery(a
+#     "notification_app",
+#     broker=os.getenv("CELERY_BROKER_URL"),
+#     backend=os.getenv("CELERY_BACKEND_URL")
+# )
 
 # ---------------- CONFIGURE TIMEZONE ----------------
 celery.conf.update(
